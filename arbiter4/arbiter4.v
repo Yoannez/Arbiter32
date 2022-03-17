e2d11ef653e457388e7ea77e619f14b42fa18d9e
Yoannez <qichao.hu@foxmail.com>
2022-03-17 21:14:28 +0100

    Change the structure of arbiter4

--- arbiter4/arbiter4.v
+++ arbiter4/arbiter4.v
@@ -1,48 +1,162 @@
-module arbiter4(
-	input clk,rst,
-	input [3:0] req,
-	output reg [3:0] grant
-);
-
+module arbiter4
+	#(
+		parameter DW = 4
+	)
+	(
+		input clk,
+		input ready_in,
+		input [3:0] valid_in,
+		input [4*DW-1:0] data_in,
+		output reg [3:0] ready_out,
+		output valid_out,
+		output reg [DW-1:0] data_out
+	);
+	
+	
+assign valid_out = (valid_in==4'b0000) ? 1'b0 : 1'b1;
+	
 always@(posedge clk) begin
-	if(rst == 1'b1)
-		grant <= 4'b0000;
+	if(ready_in == 1'b0) begin
+		ready_out <= 4'b0000;
+		data_out  <= data_in[DW-1:0];
+	end
 	else begin
-		case(grant)
-			4'b0000: grant <= req & ~(req-1'b1);
-			4'b0001: casez(req)
-						4'b0000: grant <= 4'b0000;
-						4'b0001: grant <= 4'b0001;
-						4'b??1?: grant <= 4'b0010;
-						4'b?10?: grant <= 4'b0100;
-						4'b100?: grant <= 4'b1000;
-						default: grant <= 4'b0000;
+		case(ready_out)
+			4'b0000: casez(valid_in)
+						4'b0000: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b???1: begin
+									ready_out <= 4'b0001;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b??10: begin
+									ready_out <= 4'b0010;
+									data_out  <= data_in[2*DW-1:DW];
+								 end
+						4'b?100: begin
+									ready_out <= 4'b0100;
+									data_out  <= data_in[3*DW-1:2*DW];
+								 end
+						4'b1000: begin
+									ready_out <= 4'b1000;
+									data_out  <= data_in[4*DW-1:3*DW];
+								 end
+						default: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
 					endcase
-			4'b0010: casez(req)
-						4'b0000: grant <= 4'b0000;
-						4'b0010: grant <= 4'b0010;
-						4'b?1??: grant <= 4'b0100;
-						4'b10??: grant <= 4'b1000;
-						4'b00?1: grant <= 4'b0001;
-						default: grant <= 4'b0000;
+			//ready_out <= valid_in & ~(valid_in-1'b1); //找出valid_in第一个1的位置
+			4'b0001: casez(valid_in)
+						4'b0000: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b0001: begin
+									ready_out <= 4'b0001;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b??1?: begin
+									ready_out <= 4'b0010;
+									data_out  <= data_in[2*DW-1:DW];
+								 end
+						4'b?10?: begin
+									ready_out <= 4'b0100;
+									data_out  <= data_in[3*DW-1:2*DW];
+								 end
+						4'b100?: begin
+									ready_out <= 4'b1000;
+									data_out  <= data_in[4*DW-1:3*DW];
+								 end
+						default: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
 					endcase
-			4'b0100: casez(req)
-						4'b0000: grant <= 4'b0000;
-						4'b0100: grant <= 4'b0100;
-						4'b1???: grant <= 4'b1000;
-						4'b0??1: grant <= 4'b0001;
-						4'b0?10: grant <= 4'b0010;
-						default: grant <= 4'b0000;
+			4'b0010: casez(valid_in)
+						4'b0000: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b0010: begin
+									ready_out <= 4'b0010;
+									data_out  <= data_in[2*DW-1:DW];
+								 end
+						4'b?1??: begin
+									ready_out <= 4'b0100;
+									data_out  <= data_in[3*DW-1:2*DW];
+								 end
+						4'b10??: begin
+									ready_out <= 4'b1000;
+									data_out  <= data_in[4*DW-1:3*DW];
+								 end
+						4'b00?1: begin
+									ready_out <= 4'b0001;
+									data_out  <= data_in[DW-1:0];
+								 end
+						default: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
 					endcase
-			4'b1000: casez(req)
-						4'b0000: grant <= 4'b0000;
-						4'b1000: grant <= 4'b1000;
-						4'b???1: grant <= 4'b0001;
-						4'b??10: grant <= 4'b0010;
-						4'b?100: grant <= 4'b0100;
-						default: grant <= 4'b0000;
+			4'b0100: casez(valid_in)
+						4'b0000: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b0100: begin
+									ready_out <= 4'b0100;
+									data_out  <= data_in[3*DW-1:2*DW];
+								 end
+						4'b1???: begin
+									ready_out <= 4'b1000;
+									data_out  <= data_in[4*DW-1:3*DW];
+								 end
+						4'b0??1: begin
+									ready_out <= 4'b0001;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b0?10: begin
+									ready_out <= 4'b0010;
+									data_out  <= data_in[2*DW-1:DW];
+								 end
+						default: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
 					endcase
-			default: grant <= 4'b0000;
+			4'b1000: casez(valid_in)
+						4'b0000: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b1000: begin
+									ready_out <= 4'b1000;
+									data_out  <= data_in[4*DW-1:3*DW];
+								 end
+						4'b???1: begin
+									ready_out <= 4'b0001;
+									data_out  <= data_in[DW-1:0];
+								 end
+						4'b??10: begin
+									ready_out <= 4'b0010;
+									data_out  <= data_in[2*DW-1:DW];
+								 end
+						4'b?100: begin
+									ready_out <= 4'b0100;
+									data_out  <= data_in[3*DW-1:2*DW];
+								 end
+						default: begin
+									ready_out <= 4'b0000;
+									data_out  <= data_in[DW-1:0];
+								 end
+					endcase
+			default: begin
+						ready_out <= 4'b0000;
+						data_out  <= data_in[DW-1:0];
+					 end
 		endcase
 	end
 end
